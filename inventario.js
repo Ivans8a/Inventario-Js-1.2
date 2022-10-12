2cbcfbd85af90efbaecf99cb2d6e8c900acf5e7c
Ivan8a <ivan8a>
2022-10-11 22:40:33 -0500

    Intento de resolver listarInverso #1

---
+++ inventario.js
@@ -0,0 +1,54 @@
+class Nodo {
+    constructor(numero) {
+        this.numero = numero;
+        this.next = null;
+    }
+}
+class Lista {
+    constructor() {
+        this.primero = null;
+    }
+    agregar(nuevo) {
+        if (this.primero == null)
+            this.primero = nuevo;
+        else {
+            let temp = this.primero;
+            while (temp.next != null)
+                temp = temp.next;
+            temp.next = nuevo;
+        }
+    }
+
+    agregarInicio(nuevo) {
+        nuevo.next = this.primero;
+        this.primero = nuevo
+
+    }
+
+    listarInverso() {
+            if (this.primero = null) {
+                return "";
+            } else {
+                this.recListar(this.primero)
+            }
+        }
+        /*Al probar el primer if me dice que no puede leer la propiedad .next de un null, ¿como podria poner esa condicion?*/
+    recListar(nodoX) {
+        if (nodoX.next == null) {
+            return `${nodoX}`
+        }
+    }
+}
+
+let datos = new Lista();
+let num = new Nodo(2);
+datos.agregar(num);
+/*num = new Nodo(3);
+datos.agregar(num);
+num = new Nodo(4);
+datos.agregar(num);
+num = new Nodo(5);
+datos.agregar(num);
+num = new Nodo(1);
+datos.agregarInicio(num);*/
+console.log(datos.listarInverso());
