8dc781215b2866c12addb73e6d66efdc838b11c1
Ivan8a <isantoyo0@ucol.mx>
2022-10-13 16:50:07 -0500

    Trabajo en clase 13/10/2022

--- inventario.js
+++ inventario.js
@@ -26,29 +26,51 @@ class Lista {
     }
 
     listarInverso() {
-            if (this.primero = null) {
+            if (this.primero == null) {
                 return "";
             } else {
-                this.recListar(this.primero)
+                return this.recListar(this.primero)
             }
         }
         /*Al probar el primer if me dice que no puede leer la propiedad .next de un null, ¿como podria poner esa condicion?*/
     recListar(nodoX) {
         if (nodoX.next == null) {
-            return `${nodoX}`
+            return `${nodoX.numero}`
+        } else {
+            return this.recListar(nodoX.next) + "  " + nodoX.numero  ;
         }
     }
+
+    listar(){
+        let res="";
+        let temp=this.primero;
+        while(temp!=null){
+            res+=temp.numero + "  ";
+            temp=temp.next;
+        }
+        return res;
+      }
+    
+      extraerPrimero(){
+        let aux = this.primero
+        this.primero = this.primero.next;
+        return aux
+      }
+    
+      extraerUltimo(){
+        
+      }
 }
 
 let datos = new Lista();
 let num = new Nodo(2);
 datos.agregar(num);
-/*num = new Nodo(3);
+num = new Nodo(3);
 datos.agregar(num);
 num = new Nodo(4);
 datos.agregar(num);
 num = new Nodo(5);
 datos.agregar(num);
 num = new Nodo(1);
-datos.agregarInicio(num);*/
+datos.agregarInicio(num);
 console.log(datos.listarInverso());
